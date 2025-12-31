83e95cdb796f3b8668a599ff9c44b389a1cae7a4
Sazid Ahmed <sazidahmed0912@gmail.com>
2025-12-31 19:18:04 +0530

    Add CartContext for better state management

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CartProvider } from "./context/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <CartProvider>
     <App />
-  </React.StrictMode>
+  </CartProvider>
 );
